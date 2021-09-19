978460a36094f686706065b02f8c6d029d68de13
Victor Ivan <viictor.ivan@gmail.com>
2021-09-19 16:50:30 -0300

    mvp ?

--- ft_memset.c
+++ ft_memset.c
@@ -6,10 +6,11 @@
 /*   By: victor <vivan-de@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 14:42:51 by victor            #+#    #+#             */
-/*   Updated: 2021/08/20 14:43:17 by victor           ###   ########.fr       */
+/*   Updated: 2021/09/19 16:49:43 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+
 void	*ft_memset(void *str, int c, size_t n)
 {
 	unsigned char	*dst;
